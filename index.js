fdbb8aef886928050e0c703261bc8409bb11d958
Minecraft0Builder <93701157+Minecraft0Builder@users.noreply.github.com>
2022-01-25 20:03:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,37 +10,21 @@ const voice = require("@discordjs/voice");
 const DisTube = require("distube").default;
 const https = require('https-proxy-agent');
 const client = new Discord.Client({
-    fetchAllMembers: false,
-    //restTimeOffset: 0,
-    //restWsBridgetimeout: 100,
     shards: "auto",
-    //shardCount: 5,
+    //BY: "Tomato#6966",
     allowedMentions: {
       parse: [ ],
       repliedUser: false,
     },
-    failIfNotExists: false,
     partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
     intents: [ 
         Discord.Intents.FLAGS.GUILDS,
-        //Discord.Intents.FLAGS.GUILD_MEMBERS,
         Discord.Intents.FLAGS.GUILD_MESSAGES,
         Discord.Intents.FLAGS.GUILD_VOICE_STATES,
-        //Discord.Intents.FLAGS.GUILD_BANS,
-        //Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-        //Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
-        //Discord.Intents.FLAGS.GUILD_WEBHOOKS,
-        //Discord.Intents.FLAGS.GUILD_INVITES,
-        //Discord.Intents.FLAGS.GUILD_PRESENCES,
-        //Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        //Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
-        //Discord.Intents.FLAGS.DIRECT_MESSAGES,
-        //Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-        //Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING
     ],
     presence: {
       activity: {
-        name: `+help | musicium.eu`, 
+        name: `.help | Made by Xeptron`, 
         type: "PLAYING", 
       },
       status: "online"
@@ -48,8 +32,8 @@ const client = new Discord.Client({
 });
 //BOT CODED BY: Tomato#6966
 //DO NOT SHARE WITHOUT CREDITS!
-const proxy = 'http://123.123.123.123:8080';
-const agent = https(proxy);
+//const proxy = 'http://123.123.123.123:8080';
+//const agent = https(proxy);
 const { SpotifyPlugin } = require("@distube/spotify");
 const { SoundCloudPlugin } = require("@distube/soundcloud");
 let spotifyoptions = {
@@ -72,17 +56,17 @@ client.distube = new DisTube(client, {
   //emitAddListWhenCreatingQueue: false,
   searchSongs: 0,
   youtubeCookie: config.youtubeCookie,     //Comment this line if you dont want to use a youtube Cookie 
-  nsfw: false, //Set it to false if u want to disable nsfw songs
+  nsfw: true, //Set it to false if u want to disable nsfw songs
   emptyCooldown: 25,
   ytdlOptions: {
-    requestOptions: {
-      agent
-    },
+    //requestOptions: {
+    //  agent //ONLY USE ONE IF YOU KNOW WHAT YOU DO!
+    //},
     highWaterMark: 1024 * 1024 * 64,
     quality: "highestaudio",
     format: "audioonly",
     liveBuffer: 60000,
-    dlChunkSize: 1024 * 1024 * 4,
+    dlChunkSize: 1024 * 1024 * 64,
   },
   youtubeDL: true,
   updateYouTubeDL: true,
@@ -99,22 +83,21 @@ client.slashCommands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.categories = require("fs").readdirSync(`./commands`);
 client.allEmojis = require("./botconfig/emojis.json");
-client.maps = new Map();
 
 client.setMaxListeners(100); require('events').defaultMaxListeners = 100;
 
 client.settings = new Enmap({ name: "settings",dataDir: "./databases/settings"});
 client.infos = new Enmap({ name: "infos", dataDir: "./databases/infos"});
-client.autoresume = new Enmap({ name: "autoresume", dataDir: "./databases/infos"});
+
 
 //Require the Handlers                  Add the antiCrash file too, if its enabled
 ["events", "commands", "slashCommands", settings.antiCrash ? "antiCrash" : null, "distubeEvent"]
     .filter(Boolean)
     .forEach(h => {
         require(`./handlers/${h}`)(client);
-    })
+    })|| config.token
 //Start the Bot
-client.login(config.token)
+client.login(process.env.token || config.token)
 
 /**
  * @INFO
